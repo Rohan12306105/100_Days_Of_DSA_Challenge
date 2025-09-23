c2c8b35093b3d35e78af66998723e8fa48bdcf0e
Rohan12306105 <rohangrover0610@gmail.com>
2025-09-23 10:03:02 +0530

    added codes

---
+++ Day 23/game of life.cpp
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/game-of-life/solutions/73230/c-o-1-space-o-mn-time/#:~:text=Maybe%20small%20change%20makes%20it%20more%20easy%20to%20understand.
+*/
+
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m=board.size(), n=m?board[0].size():0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int count=0;
+                for(int ss=max(i-1, 0); ss<min(i+2, m); ss++){
+                    for(int tt=max(j-1, 0); tt<min(j+2, n); tt++){
+                        count+=board[ss][tt]&1;
+                    }
+                }
+                count-=board[i][j];
+                
+                if(count==3 || (board[i][j]&&count==2))
+                    board[i][j]|=2;
+            }
+        }
+        
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                board[i][j]>>=1;
+    }
+};
