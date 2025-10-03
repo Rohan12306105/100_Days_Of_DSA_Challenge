6e772580673fb3b1b8bf04789c5bdfd73ecc803c
Rohan12306105 <rohangrover0610@gmail.com>
2025-10-03 23:05:31 +0530

    added codes

---
+++ Day 33/Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/solutions/6907687/beats-100-beginner-friendly-explanation-java-python-c-javascript
+*/
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        queue<TreeNode*> q;
+        preorder(root, q);
+        if (!q.empty()) q.pop(); // root already used
+        while (!q.empty()) {
+            root->right = q.front();
+            root->left = nullptr;
+            q.pop();
+            root = root->right;
+        }
+    }
+
+    void preorder(TreeNode* node, queue<TreeNode*>& q) {
+        if (!node) return;
+        q.push(node);
+        preorder(node->left, q);
+        preorder(node->right, q);
+    }
+};
---
+++ Day 33/Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,53 @@
+/*
+https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/solutions/2033718/c-binary-tree-super-simple-100-faster
+
+gemini for explaination
+*/
+
+
+/*
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) 
+    {
+        if (!root)
+            return root;
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        while (q.size() > 1)
+        {
+            Node* curr = q.front();
+            q.pop();
+            if (!curr)
+            {
+                q.push(NULL);
+                continue;
+            }
+            curr->next = q.front();
+            if (curr->left)
+                q.push(curr->left);
+            if (curr->right)
+                q.push(curr->right);
+        }
+        return root;
+    }
+};
