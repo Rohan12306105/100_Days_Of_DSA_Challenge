5a557b7fa557a57a8484512b3f892f013fbd37c8
Rohan12306105 <rohangrover0610@gmail.com>
2025-10-12 21:51:00 +0530

    added codes

---
+++ Day 42/Find Peak Element.cpp
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/problems/find-peak-element/solutions/3189138/best-c-3-solution-binary-search-iterative-recursive-brute-force-optimize
+*/
+
+class Solution {
+private: 
+    int recursive_binary_search(vector<int>& nums, int low, int high){
+        if(low == high){
+            return low;
+        }
+        int mid = (low + high) >> 1;
+        if(nums[mid] > nums[mid+1]){
+            return recursive_binary_search(nums, low, mid);
+        }
+        else{
+            return recursive_binary_search(nums, mid+1, high);
+        }
+    }
+public:
+    int findPeakElement(vector<int>& nums) {
+        return recursive_binary_search(nums, 0, nums.size()-1);
+    }
+};
