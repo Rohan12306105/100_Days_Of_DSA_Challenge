e0a8454eba08a4bebd7d70ac8ad44cb9ff10805c
Rohan12306105 <rohangrover0610@gmail.com>
2025-10-12 12:35:21 +0530

    added codes

---
+++ Day 41/Find Median from Data Stream.cpp
@@ -0,0 +1,26 @@
+/*
+https://leetcode.com/problems/find-median-from-data-stream/solutions/1330646/c-java-python-minheap-maxheap-solution-picture-explain-clean-concise
+*/
+
+class MedianFinder {
+public:
+    priority_queue<int> maxh;
+    priority_queue<int,vector<int>,greater<int>> minh;
+    /*
+        1) store first half in maxh, second half in minh
+        2) maxh can have 1 element more than minh
+    */
+    void addNum(int num) {
+        maxh.push(num);
+        minh.push(maxh.top());
+        maxh.pop();
+        if(minh.size()>maxh.size()){
+            maxh.push(minh.top());
+            minh.pop();
+        }
+    }
+    double findMedian() {
+        if(maxh.size()==minh.size()) return (maxh.top()+minh.top())/2.0;
+        return maxh.top();
+    }
+};
