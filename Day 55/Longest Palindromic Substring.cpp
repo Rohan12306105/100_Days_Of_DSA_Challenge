d046911b6961568500e88c527a671be0b9b6b536
Rohan12306105 <138663681+Rohan12306105@users.noreply.github.com>
2025-10-25 21:46:44 +0530

    Add files via upload

---
+++ Day 55/Longest Palindromic Substring.cpp
@@ -0,0 +1,33 @@
+/*
+https://leetcode.com/problems/longest-palindromic-substring/solutions/4212564/beats-96-49-5-different-approaches-brute-force-eac-dp-ma-recursion
+*/
+
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        
+        int max_len = 1;
+        int start = 0;
+        int end = 0;
+        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > max_len) {
+                        max_len = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+        
+        return s.substr(start, end - start + 1);
+    }
+};
