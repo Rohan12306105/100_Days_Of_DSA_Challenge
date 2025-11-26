52350fae3b73a1db57cb0fba5627f6c2d83bd43b
Rohan12306105 <rohangrover0610@gmail.com>
2025-11-26 12:51:07 +0530

    added codes

---
+++ Day 87/The k Strongest Values in an Array.cpp
@@ -0,0 +1,30 @@
+/*
+https://leetcode.com/problems/the-k-strongest-values-in-an-array/solutions/675237/c-simple-solution-using-two-pointers-by-0n6zu
+*/
+class Solution {
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end());
+        int left = 0;
+        int right = arr.size()-1;
+        int midIndex = (arr.size() -1)/2;
+        int mid = arr[midIndex];
+        
+        vector<int> answer(k);
+        int counter = 0;
+        while(counter<k){
+            int leftValue = abs(arr[left]-mid);
+            int rightValue = abs(arr[right]-mid);
+            
+            if(rightValue>=leftValue){
+                answer[counter] = arr[right];
+                right--;
+            } else {
+                answer[counter] = arr[left];
+                left++;
+            }
+            counter++;
+        }
+        return answer;
+    }
+};
---
+++ Day 87/bulb switcher.cpp
@@ -0,0 +1,10 @@
+/*
+https://leetcode.com/problems/bulb-switcher/solutions/3459106/image-explanation-easiest-to-understand-5s3j9
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+         return sqrt(n);
+    }
+};
