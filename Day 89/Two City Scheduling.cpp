836b12778516ee9c5462f2a8bba178e7ce780ac8
Rohan12306105 <rohangrover0610@gmail.com>
2025-11-28 12:37:08 +0530

    added codes

---
+++ Day 89/Two City Scheduling.cpp
@@ -0,0 +1,33 @@
+/*
+https://leetcode.com/problems/two-city-scheduling/solutions/1881010/c-explained-easy-simple-short-algorithm-8l8eg
+*/
+				// 😉😉😉😉Please upvote if it helps 😉😉😉😉
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        vector<int> diff;
+        int mincost = 0;
+        
+        for(int i = 0; i<costs.size(); ++i)
+        {
+			// cost to send every person to city A
+            mincost += costs[i][0];
+			// computing difference if person sent to city B
+            diff.push_back(costs[i][1] - costs[i][0]);
+        }
+		
+		// sort the diff vector
+        sort(diff.begin(),diff.end());
+        
+		// picking n persons having least diff
+		// sending them to city B
+        for(int i=0; i<costs.size()/2; ++i)
+        {
+            mincost += diff[i];
+        }
+        
+        return  mincost;
+        
+    }
+	// for github repository link go to my profile.
+};
