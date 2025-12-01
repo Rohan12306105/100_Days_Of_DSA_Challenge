1cfed42b2196276c350a0b04377dcdf487f3e3c9
Rohan12306105 <rohangrover0610@gmail.com>
2025-12-01 19:09:49 +0530

    added codes

--- Day 83/Longest Happy Prefix.cpp
+++ Day 83/Longest Happy Prefix.cpp
@@ -23,3 +23,29 @@ public:
         return s.substr(0, dp.back());
     }
 };
+
+class Solution {
+public:
+    string longestPrefix(string s) {
+        vector<int> lpsLength(s.length(), 0);
+        int len = 0;
+        int j = 1;
+        
+        while (j < s.length()) {
+            if (s[j] == s[len]) {
+                len++;
+                lpsLength[j] = len;
+                j++;
+            } else {
+                if (len > 0) {
+                    len = lpsLength[len - 1];
+                } else {
+                    lpsLength[j] = 0;
+                    j++;
+                }
+            }
+        }
+        
+        return s.substr(0, lpsLength[s.size() - 1]);
+    }
+};
